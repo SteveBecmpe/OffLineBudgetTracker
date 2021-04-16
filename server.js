96f1b4a711da9f394264ab15fe53f98b980c60e5
Steve Black <SteveBlackecmpe@gmail.com>
2021-04-15 22:01:36 -0400

    server.js file update again.. removed html routes

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
 
 // routes
 app.use(require("./routes/api.js"));
-app.use(require("./routes/htmlRoute"));
+// app.use(require("./routes/htmlRoute"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
